9054b18d7246654106f4f1bf3f7383339bf3abe5
Laravel Daily <info@laraveldaily.com>
2023-05-11 10:16:19 +0000

    QuickAdminPanel automatic commit

--- database/migrations/2023_05_11_000016_add_relationship_fields_to_clinics_table.php
+++ database/migrations/2023_05_11_000016_add_relationship_fields_to_clinics_table.php
@@ -11,6 +11,8 @@ class AddRelationshipFieldsToClinicsTable extends Migration
         Schema::table('clinics', function (Blueprint $table) {
             $table->unsignedBigInteger('package_ids_id')->nullable();
             $table->foreign('package_ids_id', 'package_ids_fk_8460740')->references('id')->on('packages');
+            $table->unsignedBigInteger('clinic_adminid_id')->nullable();
+            $table->foreign('clinic_adminid_id', 'clinic_adminid_fk_8466815')->references('id')->on('users');
         });
     }
 }
