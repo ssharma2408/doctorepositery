a9b113f050c23d978047a7f22eef5e10570bbc7d
Sandeep Sharma <sandeep.sharma@maven.one>
2023-05-30 12:27:54 +0530

    added role

--- app/Http/Controllers/Api/V1/Admin/RegisterController.php
+++ app/Http/Controllers/Api/V1/Admin/RegisterController.php
@@ -49,6 +49,7 @@ class RegisterController extends BaseController
             $user = Auth::user(); 
             $success['token'] =  $user->createToken('MyApp')->plainTextToken; 
             $success['name'] =  $user->name;
+            $success['role'] =  $user->roles->toArray()[0]['title'];
    
             return $this->sendResponse($success, 'User login successfully.');
         } 
