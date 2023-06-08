3cd54c9e3274746c7f2587c90b0442cef388e7c0
Sandeep Sharma <sandeep.sharma@maven.one>
2023-06-08 11:15:49 +0530

    implement staff

--- app/Http/Controllers/Api/V1/Admin/RegisterController.php
+++ app/Http/Controllers/Api/V1/Admin/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Api\V1\Admin\BaseController as BaseController;
 use App\Models\User;
 use App\Models\Clinic;
 use App\Models\Domain;
+use App\Models\Staff;
 use Illuminate\Support\Facades\Auth;
 use Validator;
 use Illuminate\Http\JsonResponse;
@@ -54,33 +55,63 @@ class RegisterController extends BaseController
 		
 		if( ! isset($request->domain)){
 			return $this->sendError('Configuartion Error', ['error'=>'Please pass domain']);
-		}		
+		}
 
-		if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+		//check if clinic admin/doctor or staff
+		if (filter_var($request->email, FILTER_VALIDATE_EMAIL)) {
+			if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
 
-            $user = Auth::user();
+				$user = Auth::user();
+				
+				$user_role = $user->roles->toArray()[0]['title'];
+				
+				if($user_role == "Clinic Admin"){
+					$clinic = Clinic::where('clinic_admin_id', $user->id)->first();
+				}else{
+					$clinic = Clinic::where('id', $request->clinic_id)->first();
+				}			
+				
+				if( ! $clinic['status']){
+					return $this->sendError('Package error', ['error'=>'No package is active']);
+				}
+				
+				$domain = Domain::where('id', $clinic->domain_id)->first();
+				
+				if($domain->name != $request->domain){
+					return $this->sendError('Authorisation error', ['error'=>'You are not authorised for this doamin']);
+				}
 
-			$clinic = Clinic::where('clinic_admin_id', $user->id)->first();
-			
-			if( ! $clinic['status']){
-				return $this->sendError('Package error', ['error'=>'No package is active']);
+				$success['token'] =  $user->createToken('MyApp')->plainTextToken; 
+				$success['name'] =  $user->name;
+				$success['role'] =  $user_role;
+				$success['postfix'] =  $clinic->prefix;
+
+				return $this->sendResponse($success, 'User login successfully.');
+			} 
+			else{
+				return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+			} 
+		}else{
+			$staff = Staff::where([
+				['username', '=', $request->email],
+				['password', '=', $request->password]				
+			])->first();
+			if(!empty($staff)){
+				
+				if($staff->clinic_id == $request->clinic_id){
+					 
+					$success['token'] =  $staff->createToken('MyApp')->plainTextToken;
+					$success['name'] =  $staff->name;
+					$success['username'] =  $staff->username;
+					$success['role'] =  'Staff';
+					return $this->sendResponse($success, 'User login successfully.');
+				}else{
+					return $this->sendError('Authorisation error', ['error'=>'You are not authorised for this doamin']);
+				}
+			}else{
+				return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
 			}
-			
-			$domain = Domain::where('id', $clinic->domain_id)->first();
-			
-			if($domain->name != $request->domain){
-				return $this->sendError('Authorisation error', ['error'=>'You are not authorised for this doamin']);
-			}
-
-            $success['token'] =  $user->createToken('MyApp')->plainTextToken; 
-            $success['name'] =  $user->name;
-            $success['role'] =  $user->roles->toArray()[0]['title'];
-            $success['postfix'] =  $clinic->prefix;
-
-            return $this->sendResponse($success, 'User login successfully.');
-        } 
-        else{ 
-            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
-        } 
+		}	
+		
     }
 }
--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -6,10 +6,11 @@ use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Sanctum\HasApiTokens;
 
 class Staff extends Model
 {
-    use SoftDeletes, HasFactory;
+    use HasApiTokens, SoftDeletes, HasFactory;
 
     public $table = 'staffs';
 
